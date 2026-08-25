7617becc76ad0d41b2fcb2fcb063d5d261ff5775
nolight132 <contact@nolight.dev>
2026-08-25 23:34:30 +0200

    vicinae: 0.26.3 -> 0.27.0

--- pkgs/by-name/vi/vicinae/package.nix
+++ pkgs/by-name/vi/vicinae/package.nix
@@ -13,6 +13,7 @@
   ninja,
   nodejs,
   npmHooks,
+  numen,
   pkg-config,
   qt6,
   stdenv,
@@ -24,13 +25,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vicinae";
-  version = "0.26.3";
+  version = "0.27.0";
 
   src = fetchFromGitHub {
     owner = "vicinaehq";
     repo = "vicinae";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Of1YU59mj9BZwQi7lbj/+XGaCs+MTGFCZ12bgcNbEu0=";
+    hash = "sha256-64RsDvhlfGSJWqI7yeE47lL05octOSX5vIPrIh8SHZ8=";
   };
 
   apiDeps = fetchNpmDeps {
@@ -50,6 +51,7 @@ stdenv.mkDerivation (finalAttrs: {
     "INSTALL_BROWSER_NATIVE_HOST" = "OFF";
     "USE_SYSTEM_CMARK_GFM" = "ON";
     "USE_SYSTEM_GLAZE" = "ON";
+    "USE_SYSTEM_NUMEN" = "ON";
     "USE_SYSTEM_KF6" = "ON";
     "USE_SYSTEM_QT_KEYCHAIN" = "ON";
     "BUNDLE_SOULVER_CORE" = "OFF";
@@ -65,6 +67,7 @@ stdenv.mkDerivation (finalAttrs: {
     cmake
     ninja
     nodejs
+    numen
     pkg-config
     qt6.qttools
     qt6.wrapQtAppsHook
@@ -81,6 +84,7 @@ stdenv.mkDerivation (finalAttrs: {
     libqalculate
     minizip
     nodejs
+    numen
     qt6.qtbase
     qt6.qtdeclarative
     qt6.qtimageformats
